c10361b6040279d3da8be0c0ef389c44b54c00f2
María Prieto Rosales <mariarosales97.mr@gmail.com>
2018-02-07 16:09:05 +0100

    Revert "Angular App v1.0"
    This reverts commit cdb712ca24cb53a5073a5320658918e3344e8157.

--- hlanz-bank/src/app/app.module.ts
+++ hlanz-bank/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
